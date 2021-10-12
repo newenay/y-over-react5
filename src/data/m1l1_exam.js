b218a94355584a024d249b5df5c3f4ad5597b59d
newenay <37123024+newenay@users.noreply.github.com>
2021-10-12 14:35:50 -0400

    Data updated to starter lesson

--- src/data/m1l1_exam.js
+++ src/data/m1l1_exam.js
@@ -1,449 +1,121 @@
 const examQuestions = {
-    "m1l1f11":[
+    "m1l1f05":[
         {
             elo: 1,
-            question: "Personnel Recovery is the sum of  _________, _________, and ________ efforts to prepare for and execute the recovery and reintegration of isolated personnel.",
+            question: "Review question 1  _________ ? ***If 'locked=true in store.js, you must pass Review or Exam with a 75%.",
             answers: [
                 {
                     type: "correct",
-                    content: "Military, Diplomatic, and Civil"
+                    content: "Correct Answer"
                 },
                 {
                     type: "distr1",
-                    content: "Department of Defense, Congress, and Senate"
+                    content: "Distractor 1"
                 },
                 {
                     type: "distr2",
-                    content: "FBI, CIA, and DOD"
+                    content: "Distractor 2"
                 },
                 {
                     type: "distr3",
-                    content: "Secretary of Defense, Secretary of State, and Presidential"
-                },
-            ]
-        },
-        {
-            elo: 1,
-            question: "Presidential Policy Directive – 30 (PPD-30), U.S. Nationals Taken Hostage Abroad and Personnel Recovery Efforts, outlines the strategic objectives, _________, _________, and ________ to isolating events to achieve the policy goals to include all USG departments and agencies.",
-            answers: [
-                {
-                    type: "distr1",
-                    content: "React, Negotiate, and Resolve"
-                },
-                {
-                    type: "correct",
-                    content: "Prevent, Prepare, and Respond"
-                },
-                {
-                    type: "distr2",
-                    content: "Tactical, Strategic, and International"
-                },
-                {
-                    type: "distr3",
-                    content: "Short Term, Immediate, and Long Term"
+                    content: "Distractor 3"
                 },
             ]
         },
         {
             elo: 2,
-            question: "A Commander can declare a PR Event if the following criteria is met; a U.S. military member is or may be in a situation where they must:  _________, __________, ___________, or  _____________.",
+            question: "Review question 2  _________ ?",
             answers: [
                 {
-                    type: "distr1",
-                    content: "Identify, Negotiate, Rescue, or Ransom"
+                    type: "correct",
+                    content: "Correct Answer"
                 },
                 {
+                    type: "distr1",
+                    content: "Distractor 1"
+                },
+            ]
+        },
+        {
+            elo: 3,
+            question: "Review question 3  _________ ?",
+            answers: [
+                {
                     type: "correct",
-                    content: "Survive, Evade, Resist, or Escape"
+                    content: "Correct Answer"
+                },
+                {
+                    type: "distr1",
+                    content: "Distractor 1"
                 },
                 {
                     type: "distr2",
-                    content: "Prevent, Plan, Prepare, or Execution"
+                    content: "Distractor 2"
                 },
                 {
                     type: "distr3",
-                    content: "Hide, Train, Flee, or Survive"
+                    content: "All of the Above (Distractor 3)"
                 },
             ]
         },
     ],
-    "m1l1f21":[
-        {
-                elo: 2,
-                question: "The DOD Personnel Recovery system is comprised of four functions: _________, __________, ___________, and _____________.",
-                answers: [
-                    {
-                        type: "distr1",
-                        content: "Identify, Negotiate, Rescue, and Return"
-                    },
-                    {
-                        type: "distr2",
-                        content: "Survive, Evade, Resist, and Escape"
-                    },
-                    {
-                        type: "correct",
-                        content: "Preparation, Planning, Execution, and Adaptation"
-                    },
-                    {
-                        type: "distr3",
-                        content: "Education, Training, Forces, and Actions"
-                    },
-                ]
-            },
-        {
-            elo: 2,
-            question: "Personnel recovery joint education and training efforts focus on three groups: _________, _________, and ________.",
-            answers: [
-                {
-                    type: "distr1",
-                    content: "Military, Diplomatic, and Civil"
-                },
-                {
-                    type: "correct",
-                    content: "Commanders and their Staff, Recovery Forces, and Individuals at risk of isolation"
-                },
-                {
-                    type: "distr2",
-                    content: "FBI, CIA, and DOD"
-                },
-                {
-                    type: "distr3",
-                    content: "Army Special Forces, Navy Seals, and Marine Corps Raiders"
-                },
-            ]
-        },
-    ],
-    "m1l1f27":[
-        {
-            elo: 2,
-            question: "While planning for a PR operation, commanders should consider the following planning factors? Threat, Physical Environment and ________.",
-            answers: [
-                {
-                    type: "distr1",
-                    content: "Foreign press"
-                },
-                {
-                    type: "correct",
-                    content: "Friendly Capabilities"
-                },
-                {
-                    type: "distr2",
-                    content: "Landing zones"
-                },
-                {
-                    type: "distr3",
-                    content: "Command and Control"
-                },
-            ]
-        },
-        {
-            elo: 2,
-            question: "Individuals are trained and equipped to facilitate or conduct their own recovery using one of two methods described as:  ____________ or ___________.",
-            answers: [
-                {
-                    type: "distr1",
-                    content: "Assisted or Unassisted"
-                },
-                {
-                    type: "distr2",
-                    content: "Overt or Covert"
-                },
-                {
-                    type: "correct",
-                    content: "Unassisted or Opportune"
-                },
-                {
-                    type: "distr3",
-                    content: "Supervised or Unsupervised"
-                },
-            ]
-        },
-        {            
-            elo: 2,
-            question: "The Civil PR Option may include _____________ or _____________ intervention by intergovernmental organizations, nongovernmental organizations, influential persons and/or private citizens.",
-            answers: [
-                {
-                    type: "distr1",
-                    content: "Requested or Approved"
-                },
-                {
-                    type: "distr2",
-                    content: "Negotiated or Paid"
-                },
-                {
-                    type: "distr3",
-                    content: "Directed or Requested"
-                },
-                {
-                    type: "correct",
-                    content: "Sanctioned or Unsanctioned"
-                },
-            ]
-        },
-        {            
-            elo: 2,
-            question: "When gaps are identified, the commander can direct action to resolve the shortfall, develop requirements for higher headquarters (HQ) to resolve, or ______________________.",
-            answers: [
-                {
-                    type: "distr1",
-                    content: "request multinational support"
-                },
-                {
-                    type: "correct",
-                    content: "accept the risk"
-                },
-                {
-                    type: "distr2",
-                    content: "ignore the shortfall"
-                },
-                {
-                    type: "distr3",
-                    content: "plan around the shortfall"
-                },
-            ]
-        },
-    ],    
-    "m1l1f34":[
-        {
-            elo: 2,
-            question: "The report task begins with the recognition of an __________ event and ends when appropriate C2 authorities are informed.",
-            answers: [
-                {
-                    type: "correct",
-                    content: "isolating"
-                },
-                {
-                    type: "distr1",
-                    content: "kidnapping"
-                },
-                {
-                    type: "distr2",
-                    content: "act of war"
-                },
-                {
-                    type: "distr3",
-                    content: "validated"
-                },
-            ]
-        },
-        {
-            elo: 2,
-            question: "The recover task begins when planning is initiated for recovering an IP and ends when the recovery element hands off the formerly isolated person to the ________________.",
-            answers: [
-                {
-                    type: "distr1",
-                    content: "host nation"
-                },
-                {
-                    type: "correct",
-                    content: "reintegration team"
-                },
-                {
-                    type: "distr2",
-                    content: "rescue forces"
-                },
-                {
-                    type: "distr3",
-                    content: "special forces"
-                },
-            ]
-        },
-        {
-            elo: 2,
-            question: "Adaptation is dependent on real-time _______________ of ongoing operations, plus the collection of PR information and data from after action reports (AARs), PR mission logs, debriefings, and oral interviews.",
-            answers: [
-                {
-                    type: "distr1",
-                    content: "Intelligence"
-                },
-                {
-                    type: "distr2",
-                    content: "Information"
-                },
-                {
-                    type: "distr3",
-                    content: "Reviews"
-                },
-                {
-                    type: "correct",
-                    content: "Observations"
-                },
-            ]
-        },
-    ],    
-    "m1l1f36":[
+    "m1l1f07":[
         {
             elo: 1,
-            question: "Personnel Recovery is the sum of  _________, _________, and ________ efforts to prepare for and execute the recovery and reintegration of isolated personnel.",
-            answers: [
-                {type: "correct", content: "Military, Diplomatic, and Civil"},
-                {type: "distr1", content: "Department of Defense, Congress, and Senate"},
-                {type: "distr2", content: "FBI, CIA, and DOD"},
-                {type: "distr3", content: "Secretary of Defense, Secretary of State, and Presidential"},
-            ]
-        },
-        {
-            elo: 1,
-            question: "Presidential Policy Directive 30 (PPD-30), U.S. Nationals Taken Hostage Abroad and Personnel Recovery Efforts, outlines the strategic objectives, _________, _________, and ________ to isolating events to achieve the policy goals to include all USG departments and agencies.",
-            answers: [
-                {type: "distr1", content: "React, Negotiate, and Resolve"},
-                {type: "correct", content: "Prevent, Prepare, and Respond"},
-                {type: "distr2", content: "Tactical, Strategic, and International"},
-                {type: "distr3", content: "Short Term, Immediate, and Long Term"},
-            ]
-        },
-        {
-            elo: 2,
-            question: "The DOD Personnel Recovery system is comprised of four functions: _________, __________, ___________, and _____________.",
+            question: "Exam question 1  _________ ?",
             answers: [
                 {
+                    type: "correct",
+                    content: "Correct Answer"
+                },
+                {
                     type: "distr1",
-                    content: "Identify, Negotiate, Rescue, and Return"
+                    content: "Distractor 1"
                 },
                 {
                     type: "distr2",
-                    content: "Survive, Evade, Resist, and Escape"
-                },
-                {
-                    type: "correct",
-                    content: "Preparation, Planning, Execution, and Adaptation"
+                    content: "Distractor 2"
                 },
                 {
                     type: "distr3",
-                    content: "Education, Training, Forces, and Actions"
+                    content: "Distractor 3"
                 },
             ]
         },
         {
             elo: 2,
-            question: "Personnel recovery joint education and training efforts focus on three groups: _________, _________, and ________.",
+            question: "Exam question 2  _________ ?",
             answers: [
                 {
-                    type: "distr1",
-                    content: "Military, Diplomatic, and Civil"
-                },
-                {
                     type: "correct",
-                    content: "Commanders and their Staff, Recovery Forces, and Individuals at risk of isolation"
+                    content: "Correct Answer"
                 },
                 {
-                    type: "distr2",
-                    content: "FBI, CIA, and DOD"
-                },
-                {
-                    type: "distr3",
-                    content: "Army Special Forces, Navy SEALS, and Marine Corps Raiders"
+                    type: "distr1",
+                    content: "Distractor 1"
                 },
             ]
         },
         {
-            elo: 2,
-            question: "The DOD PR system is centered on the five PR execution tasks: __________ , __________ , ___________ , ___________ , and __________.",
-            answers: [
-                {
-                    type: "distr1",
-                    content: "Preparation, Planning, Locate, Execution, and Adaptation"
-                },
-                {
-                    type: "distr2",
-                    content: "Policy, Training, Civil, Diplomatic, and Military"
-                },
-                {
-                    type: "correct",
-                    content: "Report, Locate, Support, Recover, and Reintegrate"
-                },
-                {
-                    type: "distr3",
-                    content: "Report, Survive, Evade, Resist, and Escape"
-                },
-            ]
-        },
-        {
-            elo: 2,
-            question: "The report task begins with the recognition of an __________ event and ends when appropriate command authorities are informed.",
+            elo: 3,
+            question: "Exam question 3  _________ ? ***if on the last frame of the lesson, will trigger a SCORM completion when in a SCORM envir.",
             answers: [
                 {
                     type: "correct",
-                    content: "isolating"
+                    content: "Correct Answer"
                 },
                 {
                     type: "distr1",
-                    content: "kidnapping"
+                    content: "Distractor 1"
                 },
                 {
                     type: "distr2",
-                    content: "act of war"
+                    content: "Distractor 2"
                 },
                 {
                     type: "distr3",
-                    content: "validated"
+                    content: "All of the Above (Distractor 3)"
                 },
-            ]
-        },
-        {
-            elo: 2,
-            question: "The locate task involves the effort taken to find and confirm the identity of isolated personnel. It starts upon recognition of an isolating event and continues until the isolated person is ________.",
-            answers: [
-                {
-                    type: "distr1",
-                    content: "identified"
-                },
-                {
-                    type: "distr2",
-                    content: "Authenticated"
-                },
-                {
-                    type: "distr3",
-                    content: "declared dead"
-                },
-                {
-                    type: "correct",
-                    content: "Recovered"
-                },
-            ]
-        },
-        {
-            elo: 2,
-            question: "The recover task begins when planning is initiated for recovering an IP and ends when the recovery element hands off the formerly isolated person to the ________________.",
-            answers: [     
-                {
-                    type: "distr1",
-                    content: "host nation"
-                },
-                {
-                    type: "correct",
-                    content: "reintegration team"
-                },
-                {
-                    type: "distr2",
-                    content: "rescue forces"
-                },
-                {
-                    type: "distr3",
-                    content: "special forces"
-                },
-            ]
-        },
-        {
-            elo: 2,
-            question: "The reintegrate task begins when the _______________ is received and there is a possibility the recovered person will enter the reintegration process.",
-            answers: [
-                {
-                    type: "distr1",
-                    content: "Authentication notification"
-                },
-                {
-                    type: "distr2",
-                    content: "Rescue notification"
-                },
-                {
-                    type: "correct",
-                    content: "Report notification"
-                },
-                {
-                    type: "distr3",
-                    content: "Escape notification"
-                },    
             ]
         },
     ],
